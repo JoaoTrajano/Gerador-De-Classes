3fc9ee2149ee38132145358c176546749093d78b
João Trajano de Souza Neto <47094056+JoaoTrajano@users.noreply.github.com>
2020-08-18 18:29:39 -0300

    teste

--- model/VisaoUsuario.class.php
+++ model/VisaoUsuario.class.php
@@ -49,7 +49,7 @@ class VisaoUsuario
                                 <div class="form-group">
                                     <label for="usuario"><span style="float:left;"><img src="http://localhost/plataforma/_img/login.jpg" alt="user" width="45"><span></label>
                                     <input class="form-control" "type="text" id="usuario" name ="usuario_login" 
-                                    placeholder="Digite o seu usu&aacute;rio." />
+                                    placeholder="Digite o seu usu&aacute;rio. teste" />
                                     <br>
                                     <label for="usuario_senha"><span style="float:left;"><img src="http://localhost/plataforma/_img/cadeado.png" alt="user" width="45"><span></label> 
                                     <input class="form-control" type="password" id="usuario_senha" name ="usuario_senha" 
